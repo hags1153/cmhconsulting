6ea41fb450e63320dc9f4decb64b658252d2fa3b
azrh <rhags1134@gmail.com>
2025-10-09 14:17:02 -0500

    Configure Web3Forms: add provided access key to contact config

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,5 +3,5 @@
 // 2) Add your email (info@cmh365.com) and create a form to get an access key
 // 3) Paste the access key below
 window.CONTACT = {
-  web3formsKey: "", // e.g., "abcdef12-3456-7890-abcd-ef1234567890"
+  web3formsKey: "22d12d93-b92e-4431-9568-84e9d5082be6", // Web3Forms access key (recipient set in Web3Forms)
 };
